e5c4ff971251d59182e47be9cc7ab235b417cf96
théoo <90846584+theokbokki@users.noreply.github.com>
2025-01-19 18:15:36 +0100

    Add favicons and social img

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,13 @@
 import axios from 'axios';
+import.meta.glob([
+    '../img/**',
+    '../fonts/**',
+    '../favicons/**.svg',
+    '../favicons/**.png',
+    '../favicons/**.ico',
+    '../favicons/**.webmanifest',
+]);
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
